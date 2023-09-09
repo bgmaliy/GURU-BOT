65f80245e9b4f4e2434959acae37003fba371f60
bgmaliy <100511573+bgmaliy@users.noreply.github.com>
2023-09-10 01:18:43 +0400

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['994555647159', 'me', true],
+  ['994555647159', 'Bgmaliy', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.mods = ['994555647159','994555647159'] 
+global.prems = ['994555647159']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,16 +47,16 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '𝙱𝙶𝙼𝙰𝙻𝙸̇𝚈-𝙱𝙾𝚃-𝚅1'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.packname = '𝚂𝙷𝙸̇𝚁𝚅𝙰𝙽' 
+global.author = '@𝚜𝚑𝚒𝚛𝚟𝚊𝚗' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/sirvan.on2\n' 
+global.dygp = 'https://'
+global.fgsc = 'https://github.com/1' 
+global.fgyt = 'https://youtube.com/@u'
+global.fgpyp = 'https://youtube.com/ru'
+global.fglog = 'https://raw.githubuserco.jpg' 
 
 
 global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
